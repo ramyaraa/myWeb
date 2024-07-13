36ba9a60e8a98008309106f3f8e08f2df5469d81
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-13 08:31:30 +0300

    create a dictionary , and add another def for number of months, use redirect response use int and str in path

--- challenges/views.py
+++ challenges/views.py
@@ -1,25 +1,57 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 
 
-def january(request):
-    return HttpResponse("in January eat apples") 
+month_dict = {
+    "january": "in January eat apples",
+    "february": "in February eat bananas",
+    "march": "in March eat oranges",
+    "april": "in April eat pineapples",
+    "may": "in May eat mangos",
+    "june": "in June eat watermelons",
+    "july": "in July eat grapes",
+    "august": "in August eat strawberries",
+    "september": "in September eat blueberries",
+    "october": "in October eat peaches",
+    "november": "in November eat plums",
+    "december": "in December eat dates",
+}
+# def monthly_challenge_by_number(request, month):
+#     months = {
+#         1: "january",
+#         2: "february",
+#         3: "march",
+#         4: "april",
+#         5: "may",
+#         6: "june",
+#         7: "july",
+#         8: "august",
+#         9: "september",
+#         10: "october",
+#         11: "november",
+#         12: "december",
+#     }
+#     if month in months:
+#         redirect_month = months[month]
+#         #redirect it means redirect to another page forexample you type 3 it will redirect you to march
+#         return HttpResponseRedirect(redirect_month)
+#     else:
+#         return HttpResponseNotFound("Invalid month")
 
-def february(request):
-    return HttpResponse("in February eat bananas")
+def monthly_challenge_by_number(request, month):
+    months = list(month_dict.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    redirect_month = months[month - 1]
+    # you can use this
+    # return HttpResponseRedirect(redirect_month)
+    # or this     it is same thing
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def monthly_challenge(request, month):
-    if month == "january":
-        text ="in January eat apples"
-    elif month == "february":
-        text ="in February eat bananas"
-    elif month == "march":
-        text ="in March eat oranges"
-    elif month == "april":
-        text ="in April eat pineapples"
-    elif month == "may":
-        text ="in May eat watermelons"
-    else:
+    try:
+        text = month_dict[month]
+        return HttpResponse(text)
+    except:
         return HttpResponseNotFound("Invalid month")
-    return HttpResponse(text)
 
