7287a36fde7cb5205ebda308b28e7060dbb1f72f
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-13 07:55:21 +0300

    add all month by using <> varible in path

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january, name="january"),
-    path("february", views.february, name="february"),
+    path("<month>", views.monthly_challenge, name="monthly_challenge"),
 
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 
 def january(request):
@@ -7,4 +7,19 @@ def january(request):
 
 def february(request):
     return HttpResponse("in February eat bananas")
+
+def monthly_challenge(request, month):
+    if month == "january":
+        text ="in January eat apples"
+    elif month == "february":
+        text ="in February eat bananas"
+    elif month == "march":
+        text ="in March eat oranges"
+    elif month == "april":
+        text ="in April eat pineapples"
+    elif month == "may":
+        text ="in May eat watermelons"
+    else:
+        return HttpResponseNotFound("Invalid month")
+    return HttpResponse(text)
 
