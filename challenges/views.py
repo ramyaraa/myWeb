1dfbe7a201f21cbc4de0167cc36d4f71866de4b0
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-13 10:31:36 +0300

    reverse functiion

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 
 
 month_dict = {
@@ -16,37 +17,15 @@ month_dict = {
     "november": "in November eat plums",
     "december": "in December eat dates",
 }
-# def monthly_challenge_by_number(request, month):
-#     months = {
-#         1: "january",
-#         2: "february",
-#         3: "march",
-#         4: "april",
-#         5: "may",
-#         6: "june",
-#         7: "july",
-#         8: "august",
-#         9: "september",
-#         10: "october",
-#         11: "november",
-#         12: "december",
-#     }
-#     if month in months:
-#         redirect_month = months[month]
-#         #redirect it means redirect to another page forexample you type 3 it will redirect you to march
-#         return HttpResponseRedirect(redirect_month)
-#     else:
-#         return HttpResponseNotFound("Invalid month")
+
 
 def monthly_challenge_by_number(request, month):
     months = list(month_dict.keys())
     if month > len(months):
         return HttpResponseNotFound("Invalid month")
     redirect_month = months[month - 1]
-    # you can use this
-    # return HttpResponseRedirect(redirect_month)
-    # or this     it is same thing
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("monthly_challenge", args=[redirect_month]) # create /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
